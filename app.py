63f999c878aea96a9b159ea93d76a2e11f0425ca
AislerMp <eislerperezmoreno@gmail.com>
2025-03-05 15:44:46 -0600

    env

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
-from decouple import config
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY_FLASK")
+
 
 @app.route("/")
 def index():
