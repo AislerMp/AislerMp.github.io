db434b5b8d93fd473caf19297b6547d0ddeb19c6
AislerMp <eislerperezmoreno@gmail.com>
2025-03-05 13:57:39 -0600

    Updated

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from decouple import config
 
 app = Flask(__name__)
-    
+app.config["SECRET_KEY"] = config("SECRET_KEY_FLASK")
+
 @app.route("/")
 def index():
     return render_template("index.html")
