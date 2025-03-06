2b5ab47ecee67effa42704bee6858eb4479ce9a6
AislerMp <eislerperezmoreno@gmail.com>
2025-03-05 19:10:12 -0600

    Added Database connection

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 load_dotenv()
@@ -7,6 +8,19 @@ load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY_FLASK")
 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SECRET_SERVER")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# Definir una tabla de ejemplo
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(80), nullable=False)
+
+# Crear la base de datos en Railway (si no existe)
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
